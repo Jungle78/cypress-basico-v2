3fdeb7d9131c3dfb62c99c37550db8b32d489a07
Jungle78 <laissilva629@gmail.com>
2024-10-25 00:09:38 -0300

    25/10

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-
-  it('verifica o título da aplicação', function() {
+  beforeEach(function() {
     cy.visit('./src/index.html')
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  })
+   })
+  it('verifica o título da aplicação', function(){
+    cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+       
+   })
+
+  it.only('Preenche os campos obrigatórios e envia o formulário', function() {
+    cy.get('#firstName').type('João'); // Preencher o campo Nome
+    cy.get('#lastName').type('Silva'); // Preencher o campo Sobrenome
+    cy.get('#email').type('joao.silva@example.com'); // Preencher o campo Email
+    cy.get('#open-text-area').type('Gostaria de mais informações sobre os seus serviços.'); // Preencher o campo "Como podemos te ajudar?"
+    
+    cy.get('button[type="submit"]').click();
+
+   })
 })
